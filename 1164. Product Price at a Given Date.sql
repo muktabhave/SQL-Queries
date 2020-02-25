9d6a35c3b8509084c17e3c243f364804d46ecc00
Mukta Girkar <muktagirkar@gmail.com>
2020-02-24 17:44:42 -0800

    Create 1164. Product Price at a Given Date.sql

---
+++ 1164. Product Price at a Given Date.sql
@@ -0,0 +1,15 @@
+select product_id,
+case when change_date=to_date('2019-08-16') then new_price
+     when change_date> to_date('2019-08-16') and prev_change_date is null then 10
+     when change_date> to_date('2019-08-16') and prev_change_date is not null then prev_price
+end as price from
+(select product_id, new_price, change_date,
+lag(change_date, 1) over (partition by product_id order by change_date) as prev_change_date,
+lag(new_price, 1) over (partition by product_id order by change_date) as prev_price
+from products) 
+where 
+case when change_date=to_date('2019-08-16') then new_price
+     when change_date> to_date('2019-08-16') and prev_change_date is null then 10
+     when change_date> to_date('2019-08-16') and prev_change_date is not null then prev_price
+end is not null
+order by price desc
