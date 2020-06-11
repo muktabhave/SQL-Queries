ffe3991b966a7d2b3744ddedb193dfcf9d67cc78
Mukta Girkar <muktagirkar@gmail.com>
2020-06-10 23:59:14 -0700

    Update 1251. Average Selling Price.sql

--- 1251. Average Selling Price.sql
+++ 1251. Average Selling Price.sql
@@ -1,1 +1,10 @@
 
+/* Write an SQL query to find the average selling price for each product.
+average_price should be rounded to 2 decimal places. */
+
+select p.product_id, 
+round(sum(price*units)/ sum(units),2) as average_price 
+from prices p join unitssold u
+on (p.product_id=u.product_id
+   and purchase_date>=start_date and purchase_date<=end_date)
+group by p.product_id
