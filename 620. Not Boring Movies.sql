e57f4f1086f6f5ef858739c71cde44bb618841a4
Mukta Girkar <muktagirkar@gmail.com>
2020-06-20 00:02:18 -0700

    Update 620. Not Boring Movies.sql

--- 620. Not Boring Movies.sql
+++ 620. Not Boring Movies.sql
@@ -1,1 +1,8 @@
+/* X city opened a new cinema, many people would like to go to this cinema. The cinema also gives out a poster indicating the movies’ ratings and descriptions.
+Please write a SQL query to output movies with an odd numbered ID and a description that is not 'boring'. Order the result by rating. */
 
+select id, movie, description, rating
+from cinema
+where mod(id,2)=1 
+and description!= 'boring'
+order by rating desc
