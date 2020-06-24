b30ada82cac251e7e2dd6ebc64fe8de79ae242ad
Mukta Girkar <muktagirkar@gmail.com>
2020-06-24 16:45:23 -0700

    Update 1132. Reported Posts II.sql

--- 1132. Reported Posts II.sql
+++ 1132. Reported Posts II.sql
@@ -1,9 +1,16 @@
-select 
-round(avg(daily_per),2) as average_daily_percent from
-(select 
- (count(case when remove_date is not null then a.post_id end)/count(a.post_id))*100 as daily_per,
-action_date
-from Actions a left outer join removals r 
-on (a.post_id=r.post_id) 
+/* Write an SQL query to find the average for daily percentage of posts that got removed after being reported as spam, rounded to 2 decimal places. 
+
+https://leetcode.com/problems/reported-posts-ii/
+
+*/
+
+select round(avg(avg_percent),2) as average_daily_percent from
+(select
+action_date,
+(count(distinct case when r.remove_date>= a.action_date then a.post_id end) 
+ /
+count( distinct a.post_id))*100 as avg_percent 
+from actions a left outer join removals r
+on (a.post_id= r.post_id)
 where action='report' and extra='spam'
-group by action_date)
+group by action_date) 
