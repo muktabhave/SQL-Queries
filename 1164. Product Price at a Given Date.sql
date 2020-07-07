54a84aeb8ba77d5cc5fe3d47e5085ca29f5dd517
Mukta Girkar <muktagirkar@gmail.com>
2020-07-07 14:14:08 -0700

    Update 1164. Product Price at a Given Date.sql

--- 1164. Product Price at a Given Date.sql
+++ 1164. Product Price at a Given Date.sql
@@ -13,3 +13,20 @@ case when change_date=to_date('2019-08-16') then new_price
      when change_date> to_date('2019-08-16') and prev_change_date is not null then prev_price
 end is not null
 order by price desc
+
+
+/* Oher Method */
+
+/* Write your PL/SQL query statement below */
+
+select product_id, 
+case when change_date> '2019-08-16' then 10 else new_price end as price from products
+where (product_id, change_date) in
+(select product_id, max(change_date) as change_date
+from products
+where change_date<= '2019-08-16'
+group by product_id
+union
+select product_id , min(change_date) as change_date from products 
+group by product_id
+having min(change_date)> '2019-08-16')
