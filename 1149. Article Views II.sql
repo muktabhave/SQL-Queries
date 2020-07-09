d25f3a5e064349e7d4c37a4e22466f9308bad50d
Mukta Girkar <muktagirkar@gmail.com>
2020-07-09 01:04:02 -0700

    Create 1149. Article Views II.sql

---
+++ 1149. Article Views II.sql
@@ -0,0 +1,7 @@
+/* Write an SQL query to find all the people who viewed more than one article on the same date, sorted in ascending order by their id. */
+
+select distinct viewer_id as id
+from views 
+group by viewer_id,view_date
+having count(distinct article_id)>1)
+order by viewer_id
