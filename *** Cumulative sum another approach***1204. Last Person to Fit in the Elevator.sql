365a4713446b914555ad48def80a016167091bee
Mukta Girkar <muktagirkar@gmail.com>
2020-07-19 17:00:22 -0700

    Create *** Cumulative sum another approach***1204. Last Person to Fit in the Elevator.sql

---
+++ *** Cumulative sum another approach***1204. Last Person to Fit in the Elevator.sql
@@ -0,0 +1,14 @@
+/* Write an SQL query to find the person_name of the last person who will fit in the elevator without exceeding the weight limit. It is guaranteed that the person who is first in the queue can fit in the elevator. */
+
+select person_name from Queue p3 
+join
+(select  p1.turn, sum(p2.weight) as weight,
+rank() over (order by sum(p2.weight) desc) as rn
+from 
+(select turn, weight from Queue) p1
+join (select turn, weight from Queue)  p2
+on (p1.turn>= p2.turn)
+group by p1.turn
+having sum(p2.weight)<=1000) p4
+on (p3.turn= p4.turn
+and p4.rn=1)
