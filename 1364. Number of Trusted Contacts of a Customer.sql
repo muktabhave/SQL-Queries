e1e4194ff23a07c5ef834a1cdfa148ebe864b0bf
Mukta Girkar <muktagirkar@gmail.com>
2020-07-21 21:08:50 -0700

    Update 1364. Number of Trusted Contacts of a Customer.sql

--- 1364. Number of Trusted Contacts of a Customer.sql
+++ 1364. Number of Trusted Contacts of a Customer.sql
@@ -1,13 +1,19 @@
-/* Write your PL/SQL query statement below */
+/* Write an SQL query to find the following for each invoice_id:
 
+customer_name: The name of the customer the invoice is related to.
+price: The price of the invoice.
+contacts_cnt: The number of contacts related to the customer.
+trusted_contacts_cnt: The number of contacts related to the customer and at the same time they are customers to the shop. (i.e His/Her email exists in the Customers table.)
+Order the result table by invoice_id.
+*/
 
-select invoice_id, customer_name, price, 
-count(contact_name) as contcats_cnt,
-count(case when ct.contact_email= cs.email then 1 end ) as trusted_contcats_cnt
+select invoice_id, cs.customer_name, price, count(ct.contact_name) as contacts_cnt, 
+count( case when ct.contact_email in (select email from Customers) then ct.contact_email end) as trusted_contacts_cnt
 from 
-customers cs join contacts ct
+customers cs 
+left outer join contacts ct
 on (cs.customer_id=ct.user_id)
-join invoices i
+right outer join invoices i
 on (cs.customer_id= i.user_id)
-group by invoice_id, customer_name, price
+group by invoice_id, cs.customer_name, price
 order by 1
