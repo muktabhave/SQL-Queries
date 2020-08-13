f356dbbca7c34f86c94f5a42264cfa9ab2598ea9
Mukta Girkar <muktagirkar@gmail.com>
2020-08-12 23:54:56 -0700

    Update 1159. Market Analysis II.sql

--- 1159. Market Analysis II.sql
+++ 1159. Market Analysis II.sql
@@ -1,18 +1,15 @@
 /* Write your PL/SQL query statement below */
 
 select 
-user_id,
-favorite_brand,
-
-select seller_id,item_id,
+p.seller_id,
+case when favorite_brand= p.favbrand then yes else no as 2nd_item_fav_brand
+from 
+(select seller_id, i.item_brand as favbrand from
+(select seller_id,item_id,
 row_number() over (partition by seller_id order by order_date) as rn
-from orders 
-
-from users u join orders o
-on (u.user_id= o.seller_id)
-
-
-
-
+from orders ) o
 join items i
 on (i.item_id= o.item_id)
+where o.rn=2) p
+join users u
+on (u.user_id- p.seller_id)
