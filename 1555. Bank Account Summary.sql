fb6b2d11eb0c83b91a1842767f0897d90c7268ff
Mukta Girkar <muktagirkar@gmail.com>
2020-08-27 00:33:46 -0700

    Update 1555. Bank Account Summary.sql

--- 1555. Bank Account Summary.sql
+++ 1555. Bank Account Summary.sql
@@ -1,4 +1,26 @@
-/* Write your PL/SQL query statement below */
+/* Write an SQL query to report.
+
+user_id
+user_name
+credit, current balance after performing transactions.  
+credit_limit_breached, check credit_limit ("Yes" or "No") */
+
+select
+user_id, 
+user_name, 
+(credit-debt+crdt) as credit,
+case when (credit-debt+crdt)<0 then 'Yes' else 'No' end as credit_limit_breached
+from
+(
+select
+user_id, 
+user_name, 
+credit,
+sum(nvl(case when u.user_id= t.paid_by then amount end ,0)) as debt,
+sum(nvl(case when u.user_id= t.paid_to then amount end,0)) as crdt
+from users u 
+left outer join transaction t 
+on ((u.user_id= t.paid_by) or (u.user_id= t.paid_to))
+group by user_id, user_name, credit)
 
 
-select paid
