82731fecad5cc3d3e811e84117d20ea44578ef01
Mukta Girkar <muktagirkar@gmail.com>
2020-09-11 20:06:32 -0700

    Update 1479. Sales by Day of the Week.sql

--- 1479. Sales by Day of the Week.sql
+++ 1479. Sales by Day of the Week.sql
@@ -1,1 +1,11 @@
+/* Write your PL/SQL query statement below */
 
+select
+to_char(o.order_date, 'DAY') as day,
+i.item_category,
+sum(o.quantity) as sum_item
+from orders o join items i
+on (o.item_id= c.item_id)
+group by 
+to_char(o.order_date, 'DAY'),
+i.item_category
