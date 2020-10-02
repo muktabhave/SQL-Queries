ecde74c23c432f2f3baac5645e001b2cedbad10b
Mukta Girkar <muktagirkar@gmail.com>
2020-10-02 00:37:31 -0700

    Update substr_exercise.sql

--- substr_exercise.sql
+++ substr_exercise.sql
@@ -1,3 +1,8 @@
 /* city name ends with vowel */
 select distinct city from station
 where substr(city, length(city),1) in ('a','e','i','o','u');
+
+/* order by last 3 words city name*/
+
+select name from students where marks>75 
+order by substr(name,-3), id;
