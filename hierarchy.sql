765e50643e9a91f10ddad5c0f5ae9267d36d3832
Mukta Girkar <muktagirkar@gmail.com>
2020-10-02 01:14:28 -0700

    Create hierarchy.sql

---
+++ hierarchy.sql
@@ -0,0 +1,17 @@
+/* https://www.hackerrank.com/challenges/the-company/problem */
+
+select c.company_code, c.founder,
+count(distinct l.lead_manager_code),
+count(distinct s.senior_manager_code),
+count(distinct m.manager_code),
+count(distinct e.employee_code)
+from company c join lead_manager l 
+on(c.company_code= l.company_code)
+join senior_manager s 
+on(l.lead_manager_code= s.lead_manager_code)
+join manager m 
+on(s.senior_manager_code= m.senior_manager_code)
+left outer join employee e
+on (e.manager_code=m.manager_code)
+group by c.company_code, c.founder
+order by c.company_code;
