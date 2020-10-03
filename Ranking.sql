4ae245f39d4d4277fe0cb52c7757b0a2efb1fc1a
Mukta Girkar <muktagirkar@gmail.com>
2020-10-03 13:12:49 -0700

    Create Ranking.sql

---
+++ Ranking.sql
@@ -0,0 +1,11 @@
+/* https://www.hackerrank.com/challenges/harry-potter-and-wands/problem
+Ranking based on 2 columns*/
+
+select id, age, coins_needed, power from
+(select id, age, coins_needed, power ,
+dense_rank() over (partition by age, power order by coins_needed, power desc) as wand_rank
+from Wands w join Wands_Property p
+on (w.code= p.code)
+where p.is_evil=0 )
+where wand_rank=1
+order by power desc, age desc;
