3bc412d4327ba5fbb54369207f6ef1f0bad1c776
Mukta Girkar <muktagirkar@gmail.com>
2021-06-10 02:00:08 -0700

    Update ******ConsecutiveItems.sql

--- ******ConsecutiveItems.sql
+++ ******ConsecutiveItems.sql
@@ -1,6 +1,22 @@
 /* https://leetcode.com/problems/human-traffic-of-stadium 
 601. Human Traffic of Stadium */
 
+/* using join */
+
+select s.id , to_char(s.visit_date, 'YYYY-MM-DD') as visit_date
+--,dt.visit_date as vs1, count(dt.visit_date) from
+(select id , visit_date, people from stadium where people>100) s 
+join
+(select distinct visit_date from stadium where people>100) dt
+on (s.visit_date>= dt.visit_date+2)
+group by s.id , s.visit_date
+having count(dt.visit_date)>=3
+order by s.visit_date
+
+
+
+/* Using window function */
+
 select id , to_char(visit_date, 'YYYY-MM-DD') as visit_date , people from
 (select id , visit_date , people,
 lag(id, 1) over (order by visit_date) as lag_id_1,
