baaccaeaab55751b4e9e133e2ca8f5848657c690
Mukta Girkar <muktagirkar@gmail.com>
2021-08-21 18:48:23 -0700

    Update AdvRanking.sql

--- IMP/AdvRanking.sql
+++ IMP/AdvRanking.sql
@@ -1,4 +1,19 @@
 /* 1596. The Most Frequently Ordered Products for Each Customer */
+
+Faster Solution:
+
+SELECT customer_id, p1.product_id, p2.product_name 
+FROM
+(SELECT customer_id, product_id,
+DENSE_RANK() over (partition by customer_id order by count(order_id) desc) as dr
+FROM orders 
+GROUP BY customer_id, product_id) p1
+JOIN products p2
+ON (p1.product_id= p2.product_id)
+WHERE dr=1
+
+
+/*
 select customer_id,product_id, product_name from
 (select o.customer_id,p.product_id, p.product_name,count(o.order_id),
  dense_rank() over(partition by customer_id order by count(o.order_id) desc) as dr
@@ -7,3 +22,4 @@ on (p.product_id=o.product_id)
 group by o.customer_id,p.product_id, p.product_name
 )
 where dr=1
+*/
