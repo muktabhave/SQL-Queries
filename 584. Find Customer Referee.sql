a067700bd363e46cdc80e7a4d55749acd8f152ea
Mukta Girkar <muktagirkar@gmail.com>
2023-06-06 23:28:45 -0700

    Update 584. Find Customer Referee.sql

--- 584. Find Customer Referee.sql
+++ 584. Find Customer Referee.sql
@@ -1,4 +1,9 @@
 /* Given a table customer holding customers information and the referee. */
 
+method-1
+select name from customer where nvl(referee_id, 0) <>2
+
+
+method-2
 select name from customer
 where referee_id is null or referee_id!=2
