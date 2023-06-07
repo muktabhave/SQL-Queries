73e79fc9053976f2cc606e782b71e1271b6c3086
Mukta Girkar <muktagirkar@gmail.com>
2023-06-06 20:07:27 -0700

    Update 196. Delete Duplicate Emails.sql

--- 196. Delete Duplicate Emails.sql
+++ 196. Delete Duplicate Emails.sql
@@ -1,4 +1,12 @@
+/* First Solution */
+
 DELETE p1 FROM Person p1,
     Person p2
 WHERE
     p1.Email = p2.Email AND p1.Id > p2.Id
+
+
+/* Second solution */
+
+delete from person where id not in
+(select min(id) from person group by email)
