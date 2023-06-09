83574e0988c5e92af22cd129cd0ee30ca2cce0a3
Mukta Girkar <muktagirkar@gmail.com>
2023-06-08 20:10:17 -0700

    Create 197. Rising Temperature.sql

---
+++ 197. Rising Temperature.sql
@@ -0,0 +1,9 @@
+select id from
+(select
+id,
+temperature,
+recordDate,
+lag(recordDate) OVER (order by recordDate) as prevDate,
+lag(temperature) OVER (order by recordDate) as prevTemp
+from weather)
+where temperature>prevTemp and recordDate-prevDate=1;
