644802d9d4c2081036d8c63820a9e7ae0f2e2b49
Mukta Girkar <muktagirkar@gmail.com>
2023-08-24 16:55:26 -0700

    Update and rename 1148. Article Views I.sql to Easy/1148. Article Views I.sql

--- 1148. Article Views I.sql
+++
@@ -1,6 +0,0 @@
-/* Write an SQL query to find all the authors that viewed at least one of their own articles, sorted in ascending order by their id.*/
-
-select distinct author_id as id from views 
-where author_id= viewer_id
-order by author_id
-
