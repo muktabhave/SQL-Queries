421a96a0ca818275995293f4c58031a950b9884b
Mukta Girkar <muktagirkar@gmail.com>
2023-08-24 00:27:12 -0700

    Update and rename 1581. Customer Who Visited but Did Not Make Any Transactions.sql to Easy/1581. Customer Who Visited but Did Not Make Any Transactions.sql

--- 1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++
@@ -1,6 +0,0 @@
-/* Write an SQL query to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.*/
-
-select customer_id, count(visit_id) as count_no_trans from 
-visits where visit_id not in 
-(select visit_id from transactions)
-group by customer_id
