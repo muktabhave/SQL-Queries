61332e26e57ed92bbcfc1ecda3f0cae5441a9d51
Mukta Girkar <muktagirkar@gmail.com>
2026-07-17 15:24:02 -0700

    Update and rename 1112. Highest Grade For Each Student.sql to Medium complex/1112. Highest Grade For Each Student.sql

---
+++ Medium complex/1112. Highest Grade For Each Student.sql
@@ -0,0 +1,55 @@
+/* Write a SQL query to find the highest grade with its corresponding course for each student. In case of a tie, you should find the course with the smallest course_id. The output must be sorted by increasing student_id.*/
+
+Table: Enrollments
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| course_id     | int     |
+| grade         | int     |
++---------------+---------+
+(student_id, course_id) is the primary key (combination of columns with unique values) of this table.
+grade is never NULL.
+ 
+
+Write a solution to find the highest grade with its corresponding course for each student. In case of a tie, you should find the course with the smallest course_id.
+
+Return the result table ordered by student_id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Enrollments table:
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 2          | 2         | 95    |
+| 2          | 3         | 95    |
+| 1          | 1         | 90    |
+| 1          | 2         | 99    |
+| 3          | 1         | 80    |
+| 3          | 2         | 75    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+Output: 
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 1          | 2         | 99    |
+| 2          | 2         | 95    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+
+
+ANS:
+  
+select student_id, course_id, grade from
+(select student_id, course_id, grade,
+row_number() over (partition by student_id order by grade desc,course_id) as rn
+from enrollments)
+where rn=1
