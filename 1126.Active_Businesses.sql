4973aaedd6cfcbdde2bfca9989f8311f4245720e
Mukta Girkar <muktagirkar@gmail.com>
2026-08-04 16:09:12 -0700

    Update and rename 1126.Active_Businesses.sql to Medium Complex/1126.Active_Businesses.sql

--- 1126.Active_Businesses.sql
+++
@@ -1,9 +0,0 @@
-select e.business_id
-from events e join
-(select event_type, avg(occurences) as avg_occurances
-from events 
-group by event_type) o
-on (e.event_type= o.event_type)
-where e.occurences > o.avg_occurances
-group by e.business_id
-having count(e.event_type)>1
