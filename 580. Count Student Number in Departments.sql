f7601cec7eada7f8abb7163711b06927b8eda35c
Mukta Girkar <muktagirkar@gmail.com>
2026-08-07 14:13:03 -0700

    done

--- 580. Count Student Number in Departments.sql
+++
@@ -1,7 +0,0 @@
-/* 580. Count Student Number in Departments */
-
-select d.dept_name as dept_name, count(student_id) as student_number
-from student s right outer join department d
-on (s.dept_id= d.dept_id)
-group by d.dept_name 
-order by count(student_id) desc, d.dept_name asc}
